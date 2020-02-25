e90da8e7716da985779db366a000404c88b0674d
chrismuiruriz <chrismuiruri007@gmail.com>
2020-02-25 16:59:25 +0300

    Initial Commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Code Moran - Coming Soon</title>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800&display=swap');
+</style>
+<style>
+
+body {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Open Sans', 'sans serif';
+}
+
+</style>
+</head>
+<body>
+
+<h1>CODE MORAN</h1>
+
+</body>
+</html>
